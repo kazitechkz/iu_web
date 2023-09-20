a5e05b5cd53ed3cabc52cc87484b714be8fb6c5f
idlkz <idlkz2019@gmail.com>
2023-09-20 13:44:28 +0600

    Commit login basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {SharedModule} from "./shared/shared.module";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {StoreModule} from "@ngrx/store";
 import {registerReducer} from "./shared/store/auth/register/Register.reducer";
 import {EffectsModule} from "@ngrx/effects";
@@ -12,24 +12,27 @@ import {RegisterEffect} from "./shared/store/auth/register/Register.effect";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {ErrorInterceptor} from "./core/interceptors/error.interceptor";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {loginReducer} from "./shared/store/auth/login/login.reducer";
+import {LoginEffect} from "./shared/store/auth/login/login.effect";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    SharedModule,
-    FontAwesomeModule,
-    StoreModule.forRoot({register:registerReducer}),
-    EffectsModule.forRoot([RegisterEffect]),
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        SharedModule,
+        FontAwesomeModule,
+        StoreModule.forRoot({register: registerReducer, login: loginReducer}),
+        EffectsModule.forRoot([RegisterEffect, LoginEffect]),
+    ],
+    providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
 
-  ],
-  bootstrap: [AppComponent]
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,30 +1,42 @@
-import { Component } from '@angular/core';
-import { faGoogle,faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import {Component} from '@angular/core';
+import {faGoogle, faFacebookF} from '@fortawesome/free-brands-svg-icons';
 import {FormControl, FormGroup, isFormGroup, Validators} from "@angular/forms";
+import {LoginRequest} from "../../shared/store/auth/login/loginRequest";
+import {Store} from "@ngrx/store";
+import {loginAction} from "../../shared/store/auth/login/login.action";
+import {getloginState} from "../../shared/store/auth/login/login.selector";
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  faGoogle = faGoogle;
-  faFacebookF = faFacebookF;
 
-  login_form : FormGroup = new FormGroup({
-    email: new FormControl("", [
-      Validators.required,
-      Validators.email,
-    ]),
-    password: new FormControl("", [
-      Validators.required,
-      Validators.max(4),
-      Validators.max(255),
-    ]),
-  });
+    constructor(private store: Store) {
+    }
 
-  onSubmit() {
-    // @ts-ignore
-    console.log(JSON.stringify(this.login_form.getRawValue()));
-  }
+    faGoogle = faGoogle;
+    faFacebookF = faFacebookF;
+
+    login_form: FormGroup = new FormGroup({
+        email: new FormControl("", [
+            Validators.required,
+            Validators.email,
+        ]),
+        password: new FormControl("", [
+            Validators.required,
+            Validators.max(4),
+            Validators.max(255),
+        ]),
+    });
+
+    onSubmit() {
+        let requestData = this.login_form.getRawValue() as LoginRequest;
+        this.store.dispatch(loginAction({requestData: requestData}));
+        this.store.select(getloginState).subscribe(item => {
+          console.log(item)
+        })
+    }
 
 }
