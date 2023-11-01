8135bd72df5b44b487316f7357568bb5a04be553
idlkz <idlkz2019@gmail.com>
2023-11-01 18:20:24 +0600

    Edited Tournament,Design and Added Profile

--- src/app/dashboard/profile/my-profile/my-profile.component.ts
+++ src/app/dashboard/profile/my-profile/my-profile.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {
+  getTournamentDetailSelector
+} from "../../../shared/store/tournament/getTournamentDetail/getTournamentDetail.selector";
+import {autoUnsubscribe} from "../../../core/helpers/autoUnsubscribe";
+import {getAccountState} from "../../../shared/store/user/account/account.selector";
+import {Me} from "../../../shared/models/user.model";
+import {faCrown, faEnvelope, faPencilAlt, faPhone, faUser} from "@fortawesome/free-solid-svg-icons";
+import {ImageHelper} from "../../../core/helpers/image.helper";
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
   styleUrls: ['./my-profile.component.scss']
 })
-export class MyProfileComponent {
+export class MyProfileComponent implements OnInit{
+  private _store = inject(Store);
+  private destroyRef:DestroyRef = inject(DestroyRef);
 
+  //Data
+  //@ts-ignore
+  me:Me;
+  ngOnInit(): void {
+    this.getUserInfo();
+  }
+
+  getUserInfo(){
+    this._store.select(getAccountState).pipe(autoUnsubscribe(this.destroyRef)).subscribe(item => {
+      if (item.data){
+        this.me = item.data;
+      }
+    });
+  }
+
+
+  protected readonly faEnvelope = faEnvelope;
+  protected readonly faPhone = faPhone;
+  protected readonly faCrown = faCrown;
+  protected readonly faUser = faUser;
+  protected readonly ImageHelper = ImageHelper;
+  protected readonly faPencilAlt = faPencilAlt;
 }
