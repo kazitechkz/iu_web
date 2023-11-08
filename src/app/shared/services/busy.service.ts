9a9a558fd71e41f36741185fdf96b9839abb9518
idlkz <idlkz2019@gmail.com>
2023-11-08 15:21:01 +0600

    Commit spinner fixed

--- src/app/shared/services/busy.service.ts
+++ src/app/shared/services/busy.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from "@angular/core";
+import {inject, Injectable} from "@angular/core";
 import {NgxSpinnerService} from "ngx-spinner";
 
 @Injectable({
@@ -6,24 +6,23 @@ import {NgxSpinnerService} from "ngx-spinner";
 })
 export class BusyService {
     busyRequestCount = 0;
-
-    constructor(private spinnerService: NgxSpinnerService) {
-    }
+    private spinnerService = inject(NgxSpinnerService)
 
     busy() {
         this.busyRequestCount++;
-        this.spinnerService.show(undefined, {
-            type: 'timer',
-            bdColor: 'rgba(255,255,255,0.7)',
-            color: '#333333'
-        });
+        this.spinnerService.show().then(r => null)
+        // this.spinnerService.show(undefined, {
+        //     type: 'timer',
+        //     bdColor: 'rgba(255,255,255,0.7)',
+        //     color: '#333333'
+        // });
     }
 
     idle() {
         this.busyRequestCount--;
         if (this.busyRequestCount <= 0) {
             this.busyRequestCount = 0;
-            this.spinnerService.hide();
+            this.spinnerService.hide().then(r => null);
         }
     }
 }
