0eb7a08da8e261315f35ae8148e60a4da84ca4e4
IDLKZ <idlkz2019@gmail.com>
2023-11-25 15:46:11 +0600

    Added All Working Announces

--- src/app/dashboard/step/step-detail/step-detail.component.ts
+++ src/app/dashboard/step/step-detail/step-detail.component.ts
@@ -88,34 +88,42 @@ export class StepDetailComponent implements OnInit {
     "slidesToShow": 5,
     "slidesToScroll": 1,
     "dots": true,
-    "arrows":true,
+    "arrows":false,
     "infinite": false,
     "responsive": [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
+          slidesToShow: 6,
+          slidesToScroll: 6,
           infinite: true,
-          dots: false
+          dots: false,
         }
       },
       {
         breakpoint: 900,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 5,
+          slidesToScroll: 5,
           dots: false
         }
       },
       {
-        breakpoint: 700,
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
           dots: true
         }
-      }
+      },
     ]
   };
 
