95e459eb35b09bdcf8558ab5f0c297595ea8f742
Kontraktnik <kontrkz5@gmail.com>
2024-02-06 23:49:08 +0600

    Career (90%)

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -2,14 +2,14 @@ import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {Subject} from "../../shared/models/subject.model";
 import {
   faArrowRight,
-  faBook,
+  faBook, faBoxesPacking,
   faBullseye,
-  faCheck, faCheckCircle, faChevronRight, faCircleCheck,
+  faCheck, faCheckCircle, faChevronRight, faCircleCheck, faDumbbell,
   faHeart,
-  faLanguage,
+  faLanguage, faRocket,
   faShieldAlt,
-  faUserDoctor,
-   faWandMagicSparkles, faXmark
+  faUserDoctor, faVideo,
+  faWandMagicSparkles, faXmark
 } from "@fortawesome/free-solid-svg-icons";
 import {ImageHelper} from "../../core/helpers/image.helper";
 import {
@@ -34,9 +34,9 @@ export class IndexComponent implements OnInit{
   activePlanId:number = 1;
 
   plans:{[key: number]: { title: string,price:number,months_value:number,months:string,image:string,description:string[] }} = {
-    1:{title:"1_MONTH",price:990,months_value:1,months:"1 месяц",image:"assets/images/basic_bear.png",description:["WORK_OVER_FAILURE","SUPPORT_MULTI_LANGUAGE","ANALYZE_WEAK_POSITION","FULL_STATS","SUBJECT_MAT_GRAM","SUBJECT_GRAM","SUBJECT_HISTORY","SUBJECT_PROFS"]},
-    2:{title:"3_MONTH",price:2490,months_value:3,months:"3 месяца",image:"assets/images/standard_bear.png",description:["WORK_OVER_FAILURE","SUPPORT_MULTI_LANGUAGE","ANALYZE_WEAK_POSITION","FULL_STATS","SUBJECT_MAT_GRAM","SUBJECT_GRAM","SUBJECT_HISTORY","SUBJECT_PROFS"]},
-    3:{title:"6_MONTH",price:4990,months_value:6,months:"6 месяцев",image:"assets/images/premium_bear.png",description:["WORK_OVER_FAILURE","SUPPORT_MULTI_LANGUAGE","ANALYZE_WEAK_POSITION","FULL_STATS","SUBJECT_MAT_GRAM","SUBJECT_GRAM","SUBJECT_HISTORY","SUBJECT_PROFS"]},
+    1:{title:"Подписка на 1 месяц",price:990,months_value:1,months:"1 месяц",image:"assets/images/basic_bear.png",description:["WORK_OVER_FAILURE","SUPPORT_MULTI_LANGUAGE","ANALYZE_WEAK_POSITION","FULL_STATS","SUBJECT_MAT_GRAM","SUBJECT_GRAM","SUBJECT_HISTORY","SUBJECT_PROFS"]},
+    2:{title:"Подписка на 3 месяца",price:2490,months_value:3,months:"3 месяца",image:"assets/images/standard_bear.png",description:["WORK_OVER_FAILURE","SUPPORT_MULTI_LANGUAGE","ANALYZE_WEAK_POSITION","FULL_STATS","SUBJECT_MAT_GRAM","SUBJECT_GRAM","SUBJECT_HISTORY","SUBJECT_PROFS"]},
+    3:{title:"Подписка на 6 месяцев",price:4990,months_value:6,months:"6 месяцев",image:"assets/images/premium_bear.png",description:["WORK_OVER_FAILURE","SUPPORT_MULTI_LANGUAGE","ANALYZE_WEAK_POSITION","FULL_STATS","SUBJECT_MAT_GRAM","SUBJECT_GRAM","SUBJECT_HISTORY","SUBJECT_PROFS"]},
   }
   //Data
 
@@ -76,4 +76,8 @@ export class IndexComponent implements OnInit{
   protected readonly faBullseye = faBullseye;
   protected readonly faUserDoctor = faUserDoctor;
   protected readonly faHeart = faHeart;
+  protected readonly faRocket = faRocket;
+  protected readonly faBoxesPacking = faBoxesPacking;
+  protected readonly faDumbbell = faDumbbell;
+  protected readonly faVideo = faVideo;
 }
