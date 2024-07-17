984a8846bf686a732989a66da3a19b0f339579f0
nurba <kazitech2023@gmail.com>
2024-07-18 00:55:32 +0500

    Commit turn off wallet rating countdown

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+declare module '@ckeditor/ckeditor5-editor-classic/src/classiceditor' {
+  import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+  export default ClassicEditor;
+}
+
+declare module '@ckeditor/ckeditor5-ui/src/editorui' {
+  import EditorUI from '@ckeditor/ckeditor5-ui/src/editorui';
+  export default EditorUI;
+}
